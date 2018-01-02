108a9c08bed7f55fbff1fda5dbd580bbb859723f
Tobias Schlemmer <keinstein@users.sourceforge.net>
2018-01-02 18:33:34 +0100

    run dh_make

---
+++ debian/mingw-w64-boost.default.ex
@@ -0,0 +1,10 @@
+# Defaults for mingw-w64-boost initscript
+# sourced by /etc/init.d/mingw-w64-boost
+# installed at /etc/default/mingw-w64-boost by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+libmingw-w64-boost 1.66.0 mingw-w64-boost (>> 1.66.0-0), mingw-w64-boost (<< 1.66.0-99)
